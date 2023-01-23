49848a8340cfa04bb4dbfe1b99ef7172daab8d50
avwtr <alexvawter.ime@gmail.com>
2023-01-23 11:42:15 -0500

    Initial Commit

---
+++ capture-mvp/capture_mvpApp.swift
@@ -0,0 +1,17 @@
+//
+//  capture_mvpApp.swift
+//  capture-mvp
+//
+//  Created by Alex Vawter on 1/23/23.
+//
+
+import SwiftUI
+
+@main
+struct capture_mvpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
